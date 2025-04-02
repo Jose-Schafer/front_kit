d6c2eefeb6589403252ad5e5c0410aafe5ba3369
Jose Schafer <joseignacio.schafer@gmail.com>
2025-04-02 18:43:17 -0300

    WIP

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -10,14 +10,14 @@ export function Card({
 }) {
   console.log(gif);
   return (
-    <div className="max-w-xs w-full">
+    <div className="max-w-xl w-full">
       <div
         className={cn(
-          "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
-          "bg-[url(https://images.unsplash.com/photo-1476842634003-7dcca8f832de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80)] bg-cover",
+          "group w-full cursor-pointer overflow-hidden relative card h-64 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
+          "bg-[url(https://raw.githubusercontent.com/Jose-Schafer/front_kit/refs/heads/feature/home-enhancements/public/imgs/whatsapp-clone.png)] bg-cover",
           // Preload hover image by setting it in a pseudo-element
-          `before:bg-[url(${gif})] before:fixed before:inset-0 before:opacity-0 before:z-[-1]`,
-          `hover:bg-[url(${gif})]`,
+          "before:bg-[url(https://raw.githubusercontent.com/Jose-Schafer/front_kit/refs/heads/feature/home-enhancements/public/gifs/whatsapp-clone.gif)] before:fixed before:inset-0 before:opacity-0 before:z-[-1]",
+          "hover:bg-[url(https://raw.githubusercontent.com/Jose-Schafer/front_kit/refs/heads/feature/home-enhancements/public/gifs/whatsapp-clone.gif)]",
           "hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50",
           "transition-all duration-500",
         )}
